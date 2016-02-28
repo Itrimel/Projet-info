5bc1d79907db3ad9ee85853c013647b662a9c4dd
Tetejotups <tete531@hotmail.fr>
2016-02-28 10:41:14 +0100

    Correction de petites erreurs

--- programme principal.py
+++ programme principal.py
@@ -18,6 +18,6 @@ def Normale(Triangle(p1,p2,p3), distance):
 triangle_0=Triangle(Point(0,0,0),Point(1,0,0),Point(0,1,0))
 liste=[triangle_0]
 for i in range(nb_etapes):
-    for i in range(len(liste)):
-            distance = gauss(4**(-i),0.1**(-i))#Donne un nombre aléatoire selon une répartition gaussienne. A voir pour les paramètres ( le premier est la valeur moyenne, le second l’écart type)
+    for j in range(len(liste)):
+            distance = abs(gauss(4**(-i),0.1**(i+1)))#Donne un nombre aléatoire selon une répartition gaussienne. A voir pour les paramètres ( le premier est la valeur moyenne, le second l’écart type)
             liste=liste+[liste.pop(0).creation_triangle(distance)]#Enlève un triangle à la liste pour ajouter les trois triangles qui en sont issus
