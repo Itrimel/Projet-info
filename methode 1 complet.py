d7b91cfc0a687bc98c23e44e77ed633870c633bd
Tetejotups <tete531@hotmail.fr>
2016-03-12 18:58:26 +0100

    Ce document contient tout le code (et juste le code) pour la méthode 1

--- methode 1 complet.py
+++ methode 1 complet.py
@@ -105,12 +105,12 @@ centre_2=milieu_triangle(liste[1])
 centre_3=milieu_triangle(liste[2])
 centre=milieu_triangle(Triangle(centre_1,centre_2,centre_3))
 for i in range(6):
- 	  for j in range(len(liste)):
-	     distance = abs(gauss(4**(-i-1),0.2**(i+1)))#Donne un nombre aléatoire selon une répartition gaussienne. A voir pour les paramètres ( le premier est la valeur moyenne, le second l’écart type)
- 	     liste=liste+creation_triangle(liste.pop(0),distance,centre)#Enlève un triangle à la liste pour ajouter les trois triangles qui en sont issus
+	for j in range(len(liste)):
+		distance = abs(gauss(4**(-i-1),0.2**(i+1)))#Donne un nombre aléatoire selon une répartition gaussienne. A voir pour les paramètres ( le premier est la valeur moyenne, le second l’écart type)
+		liste=liste+creation_triangle(liste.pop(0),distance,centre)#Enlève un triangle à la liste pour ajouter les trois triangles qui en sont issus
 
 for i in range(len(liste)):#On transforme chaque élément de la liste, pour que le module d'affichage 3d puisse en faire qqchose
-	   triangle=liste[i]
- 	  liste[i]=[(triangle.point1.xP,triangle.point1.yP,triangle.point1.zP),(triangle.point2.xP,triangle.point2.yP,triangle.point2.zP),(triangle.point3.xP,triangle.point3.yP,triangle.point3.zP)]
+	triangle=liste[i]
+	liste[i]=[(triangle.point1.xP,triangle.point1.yP,triangle.point1.zP),(triangle.point2.xP,triangle.point2.yP,triangle.point2.zP),(triangle.point3.xP,triangle.point3.yP,triangle.point3.zP)]
 creation_image(liste)
 
