7c7789ce449b1740fbc9c850d52f47eac20796b1
Lavech <estelle.raguin@orange.fr>
2016-04-05 17:44:06 +0200

    Update affichage.py

--- affichage.py
+++ affichage.py
@@ -1,1 +1,10 @@
-#Contiendra le code des fenêtres Tkinter
+'''Pour afficher une image'''
+from tkinter.filedialog import *
+# On crée une fenêtre, fenêtre principale de notre interface
+fenetre = Tk()
+#La fonction askopenfilename retourne le chemin du fichier que vous avez choisi avec le nom de celui-ci.
+filepath = askopenfilename(title="Ouvrir une image",filetypes=[('png files','.png'),('all files','.*')])
+photo = PhotoImage(file=filepath)
+canvas = Canvas(fenetre, width=photo.width(), height=photo.height(), bg="yellow")
+canvas.create_image(0, 0, anchor=NW, image=photo)
+canvas.pack()
