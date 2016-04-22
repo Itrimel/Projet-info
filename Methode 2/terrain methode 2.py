d50c75e09d811e60f6b47da1a31c2a8db0a75ef7
Tetejotups <tete531@hotmail.fr>
2016-04-22 21:42:42 +0200

    Réorganisation

--- Methode 2/terrain methode 2.py
+++ Methode 2/terrain methode 2.py
@@ -51,6 +51,18 @@ class Point:#Fonctionne comme un vecteur
     def __ne__(self,other):#Teste si Point A != Point B .Retourne un booléen
         return not self==other
 
+    def dist(self,other): #distance d'un point à un autre
+        return ((self.xP - other.xP)**2+(self.yP - other.yP)**2+(self.zP-other.zP)**2)**0.5
+        
+
+def aire_tri(A,B,C):
+    a=A.dist(B)
+    b=B.dist(C)
+    c=C.dist(A)
+    s=(a+b+c)/2
+    Aire=(s*(s-a)*(s-b)*(s-c))**0.5
+    return(Aire)
+
 def creation_image(liste,save="True",chemin="ask"):
 	'''Créé le dessin, à partir d'une liste de points.
 	La variable save gère si l'image est enregistrée (Par défaut,oui), à l'emplacement "chemin". Par défaut,l'emplacement est demandé à l'utilisateur'''
@@ -80,34 +92,39 @@ def creation_image(liste,save="True",chemin="ask"):
 	Y=np.array(Y)
 	Z=np.array(Z)
 	fig=mlab.triangular_mesh(X,Y,Z,tri,colormap="gist_earth") #La figure est dessinée, avec de bonnes couleurs
-	if save: #Instructions pour la sauvegarde du fichier
-		if chemin=="ask": #Instructions pour demander à l'utilisateur l'emplacement du fichier
-			root = Tkinter.Tk()
-			root.withdraw()
-			chemin = tkFileDialog.asksaveasfilename(parent=root,initialdir="/",defaultextension="vrml",initialfile="image", title="Selectionnez le dossier d'enregistrement")
-		mlab.savefig(chemin)#Le fichier est enregistré
-		#Bloc pour modifier l'extension du fichier, et faire en sorte de ne pas effacer un autre fichier image. L'extension doit être modifiée, car, pour Blender, les fichiers VRML ont une extension en .wrl
-		nom, ext = os.path.splitext(chemin) #Le nom et l'extension du fichier sont séparés
-		i=0# Le fichier est nommé par défaut image. Pour ne pas effacer d'autres fichiers, on rajoute _i à la fin du nom, où i est un nombre. i est incrémenté jusqu'à ce que l'emplacement soit disponible
-		nom=nom+"_"
-		while os.path.isfile(nom+str(i)+".wrl"):#Instrction testant si le fichier existe, et renvoyant true si c'est le cas
-			i+=1
-		nom=nom+str(i)
-		os.rename(chemin, nom + ".wrl")#Instruction permettant de renommer le fichier "chemin" en nom +".wrl", qui est le même nom, à l'extension et un nombre à la fin près
-		#Fin du bloc
+	while 1:
+        	if save: #Instructions pour la sauvegarde du fichier
+			if chemin=="ask": #Instructions pour demander à l'utilisateur l'emplacement du fichier
+				root = Tkinter.Tk()
+				root.withdraw()
+				chemin = tkFileDialog.asksaveasfilename(parent=root,initialdir="/",defaultextension="vrml",initialfile="image", title="Selectionnez le dossier d'enregistrement")
+				if chemin=='':#Petite sécurité pour moins d'erreur
+					break
+			mlab.savefig(chemin)#Le fichier est enregistré
+			#Bloc pour modifier l'extension du fichier, et faire en sorte de ne pas effacer un autre fichier image. L'extension doit être modifiée, car, pour Blender, les fichiers VRML ont une extension en .wrl
+			nom, ext = os.path.splitext(chemin) #Le nom et l'extension du fichier sont séparés
+			i=0# Le fichier est nommé par défaut image. Pour ne pas effacer d'autres fichiers, on rajoute _i à la fin du nom, où i est un nombre. i est incrémenté jusqu'à ce que l'emplacement soit disponible
+			nom=nom+"_"
+			while os.path.isfile(nom+str(i)+".wrl"):#Instrction testant si le fichier existe, et renvoyant true si c'est le cas
+				i+=1
+			nom=nom+str(i)
+			os.rename(chemin, nom + ".wrl")#Instruction permettant de renommer le fichier "chemin" en nom +".wrl", qui est le même nom, à l'extension et un nombre à la fin près
+			#Fin du bloc
+		break
 
-def modif_rang_ajout(liste,pos,i):
+
+def modif_rang_ajout(liste,pos,i,sigma):
     '''Cette procédure permet d'ajouter des points à des lignes déjà existantes'''
     ligne=liste[pos]#On stocke la ligne sur laquelle on travaille    
     ligne2=[ligne[0]]#On crée une nouvelle ligne qui contiendra les modifications : elle contient de base le premier point qui ne bougera pas
     long=len(ligne)
     for k in range(long-1):
         point=(ligne[k]+ligne[k+1])/2.0 #Les points à ajouter sont les milieux des segments
-        point = point + Point(0,0,gauss(0,0.25**i)) #Déplace le point verticalement
+        point = point + Point(0,0,gauss(0,sigma*0.4**i)) #Déplace le point verticalement
         ligne2=ligne2+[point,ligne[k+1]]#On ajoute un point crée, puis un point existant, qui ne bouge pas
     return [ligne2]
 
-def modif_rang_creation(liste,pos,i,cote_0,cote_2,nb_etapes):
+def modif_rang_creation(liste,pos,i,cote_0,cote_2,nb_etapes,sigma):
     '''Cette procédure permet de créer une nouvelle ligne de points'''
     ligne_a=liste[pos-1] #On stocke les 2 lignes entre lesquelles sera ajouté la nouvelle ligne
     ligne_b=liste[pos]
@@ -115,26 +132,28 @@ def modif_rang_creation(liste,pos,i,cote_0,cote_2,nb_etapes):
         ligne_nouv=[cote_0[(2*pos-1)*(2**(nb_etapes-i-1))]]#On crée la nouvelle ligne avec le bon point. Il faut me croire sur parole pour la position du point da la liste ;)
     else:
         #On crée la nouvelle ligne avec le premier point, milieu du segment formé par les premiers points des 2 lignes précédentes, pouis le point est bougé
-        ligne_nouv=[(ligne_a[0]+ligne_b[0])/2.0 + Point(0,0,gauss(0,0.25**i))]
+        ligne_nouv=[(ligne_a[0]+ligne_b[0])/2.0 + Point(0,0,gauss(0,sigma*0.4**i))]
     #On crée les variables contenant les positions des points dont on veut obtenir le centre
     pos_a=0
     pos_b=1
     for k in range(pos-1):
         point1=(ligne_a[pos_a]+ligne_b[pos_b])/2.0 # Milieu du segment
-        point1 = point1 + Point(0,0,gauss(0,0.25**i)) #Déplace le point verticalement
+        point1 = point1 + Point(0,0,gauss(0,sigma*0.4**i)) #Déplace le point verticalement
         pos_a+=1 #On ajuste la position du point de la première ligne afin d'obtenir le prochain point 
         point2=(ligne_a[pos_a]+ligne_b[pos_b])/2.0
-        point2 = point2 + Point(0,0,gauss(0,0.25**i)) #Déplace le point verticalement
+        point2 = point2 + Point(0,0,gauss(0,sigma*0.4**i)) #Déplace le point verticalement
         pos_b+=1 #On ajuste la position du point de la deuxième ligne afin d'obtenir le prochain point lors du prochain passage dans la boucle
         ligne_nouv+=[point1,point2] #On ajoute les 2 points à la ligne
     if cote_2:#Même chose que pour cote_0
         ligne_nouv+=cote_2[(2*pos-1)*(2**(nb_etapes-i-1))]
     else:
-        ligne_nouv+=[(ligne_a[-1]+ligne_b[-1])/2.0 + Point(0,0,gauss(0,0.25**i))]#Ici, on prend le milieu des derniers points des lignes,d'où le -1
+        ligne_nouv+=[(ligne_a[-1]+ligne_b[-1])/2.0 + Point(0,0,gauss(0,sigma*0.4**i))]#Ici, on prend le milieu des derniers points des lignes,d'où le -1
     return [ligne_nouv]#On retourne la ligne de point sous forme de liste
 
 
 def modif_triangle(triangle,cotes_deja_faits,nb_etapes):
+    aire=aire_tri(triangle[0],triangle[1],triangle[2])
+    sigma=aire
     liste=[[triangle[0]],[triangle[1],triangle[2]]] #On crée une liste avec les 3 sommets du triangle. C'est un exemple du format utilisé : une liste de liste, chaque liste est une liste de points correspondant à une ligne du triangle
     cote_0=cotes_deja_faits[0]#On stocke dans le variables les informations pour savoir si les côtéss des triangles sont déjà faits ou pas
     cote_1=cotes_deja_faits[1]
@@ -154,15 +173,15 @@ def modif_triangle(triangle,cotes_deja_faits,nb_etapes):
         #La liste contenant le triangle précédant ne sera pas modifiée 
         for pos in range(1,len(liste)-1):#On crée toutes les nouvelles lignes, sauf les 2 dernières
             #Lors de l'ajout de nouveaux points, il se passe 2 choses : on créé de nouvelles lignes, et on ajoute des points aux lignes existantes, d'où les 2 procédures
-            liste2 = liste2 + modif_rang_creation(liste,pos,i,cote_0,cote_2,nb_etapes) + modif_rang_ajout(liste,pos,i)
+            liste2 = liste2 + modif_rang_creation(liste,pos,i,cote_0,cote_2,nb_etapes,sigma) + modif_rang_ajout(liste,pos,i,sigma)
         if cote_1:#Si ce coté a été déja fait, cote_1 est une liste et la boucle if sera activée. Si il n'a pas été fait, cote_1 contien False, le la boucle else sera activée
             dernier_cote=[cote_1[0]]
             for k in range(2**(i+1)):
                 dernier_cote=dernier_cote + [cote_1[k*(2**(nb_etapes-i-1))]]
             dernier_cote=[dernier_cote]
         else:
-            dernier_cote = modif_rang_ajout(liste,len(liste)-1,i)
-        liste2= liste2 + modif_rang_creation(liste,len(liste)-1,i,cote_0,cote_2,nb_etapes) + dernier_cote #On fait à part, car les points de la dernière ligne peuvent ou ne peuvent pas être ajoutés aléatoirement
+            dernier_cote = modif_rang_ajout(liste,len(liste)-1,i,sigma)
+        liste2= liste2 + modif_rang_creation(liste,len(liste)-1,i,cote_0,cote_2,nb_etapes,sigma) + dernier_cote #On fait à part, car les points de la dernière ligne peuvent ou ne peuvent pas être ajoutés aléatoirement
         liste=liste2 #La liste principale est modifiée, car le nouveau triangle est totalement construit
     return liste
 
